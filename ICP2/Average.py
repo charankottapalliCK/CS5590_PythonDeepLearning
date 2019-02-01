d204fc15fb4f819d8fd711bf1d5adb7280eec2ed
Charan Kottapalli <kottapallicharan@gmail.com>
2019-02-01 17:26:35 -0600

    ICP2
    code upload

---
+++ ICP2/Average.py
@@ -0,0 +1,12 @@
+n = int(input("How many  numbers  do you have? "))
+
+s = 0
+obs = map(int, raw_input().split())
+if len(obs) != n:
+    print("Did not enter "+str(n)+" values")
+else:
+    for i in range(0,len(obs)):
+        s = s+obs[i]
+    avg = s / float(n)
+    print (avg)
+
